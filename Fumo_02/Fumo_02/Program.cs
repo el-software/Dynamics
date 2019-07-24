1f95bc977e187ce3fe3bece3c7f6af5f786435ce
erosluciano <53263076+erosluciano@users.noreply.github.com>
2019-07-24 14:41:13 +0200

    Add files via upload

---
+++ Fumo_02/Fumo_02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fumo_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double prezzoPacchetto10 = 2.5;
+            double spesa = 0;
+            int giorni_trascorsi = 0;
+            DateTime fine = new DateTime(2016, 12, 25);
+            TimeSpan giorni = DateTime.Now.Date - fine;
+            giorni_trascorsi =  (int)giorni.TotalDays;
+            spesa = prezzoPacchetto10 * giorni_trascorsi ;
+            Console.WriteLine("Giorni trascorsi: " + (giorni.TotalDays).ToString());
+            Console.WriteLine("Spesa: {0} euro.", spesa);
+            Test prova = new Test();
+            prova.Stampa(giorni_trascorsi);
+        }
+        class Test
+        {
+            //int memo = 0;
+            
+            public void Stampa (int valore)
+            {
+                Console.WriteLine("Totale giorni trascorsi: {0}", valore);
+            }
+            
+        }
+    }
+    class Calcolo
+    {
+        
+    }
+}
